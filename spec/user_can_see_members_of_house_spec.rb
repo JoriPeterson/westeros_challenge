3d4821e952cde57db6a23f4bab7fb58943a76725
Jori Peterson <joripeterson@gmail.com>
2019-09-06 10:04:56 -0600

    Write test for user story

---
+++ spec/user_can_see_members_of_house_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Members of a house" do
+  it "As a user I can see members of a house" do
+
+    visit '/'
+
+    select "Greyjoy"
+
+    click_on "Get Members"
+
+    expect(current_path).to eq("/search?house=greyjoy")
+    expect(page).to have_content("House Greyjoy")
+    expect(page).to_not have_content("House Stark")
+    expect(page).to have_css(".member", count: 7)
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".id")
+    end
+  end
+end
